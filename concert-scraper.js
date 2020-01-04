bcc994832545a95c58b67eb1be816e270f496bd7
Bill Kwai <billkwai.edu@gmail.com>
2020-01-03 19:54:38 -0800

    Checks the difference in events between database and newly scraped events

--- concert-scraper.js
+++ concert-scraper.js
@@ -2,31 +2,44 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const moment = require('moment');
 
-const url = 'https://thefillmore.com/calendar';
+function scrapeFillmore() {
 
-let event_date_re = /\w{3,9}?\s\d{1,2}?,\s\d{4}?/;
-let event_time_re = /\d:\d\d\sp.m./;
-let event_price_re = /\$\d\d.\d\d/;
+    const url = 'https://thefillmore.com/calendar';
+    
+    // regular expressions for fillmore date, time, and price scraping
+    let event_date_re = /\w{3,9}?\s\d{1,2}?,\s\d{4}?/;
+    let event_time_re = /\d:\d\d\sp.m./;
+    let event_price_re = /\$\d\d.\d\d/;
 
-axios(url)
-  .then(response => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const allEvents = $('.faq_main');
-    const events = [];
-    allEvents.each(function () {
-        const title = $(this).find('.title').find('a').text();
-        const url = $(this).find('.title').find('a').attr('href');
-        const venue = 'Fillmore';
-        const otherContent = $(this).find('.content').find('p').text();
-        const event_date_raw = otherContent.match(event_date_re)[0];
-        const event_time_raw = otherContent.match(event_time_re)[0].replace('.', '');
-        const event_price_raw = otherContent.match(event_price_re)[0].replace('$', '');
+    return axios(url)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const allEvents = $('.faq_main');
+        const events = [];
+        allEvents.each(function () {
+            const title = $(this).find('.title').find('a').text();
+            const url = $(this).find('.title').find('a').attr('href');
+            const venue = 'Fillmore';
+            const otherContent = $(this).find('.content').find('p').text();
+            const event_date_raw = otherContent.match(event_date_re)[0];
+            const event_time_raw = otherContent.match(event_time_re)[0].replace('.', '');
+            const event_price_raw = otherContent.match(event_price_re)[0];
 
-        // convert date and time into datetime format
-        const event_datetime = new moment(event_date_raw + ' ' + event_time_raw, 'MMMM D YYYY h:mm a');
-        events.push({title, venue, url, date_and_time: event_datetime, price: event_price_raw});
-    });
-    console.log(events);
-  })
-  .catch(console.error);
+            // convert date and time into datetime format
+            const event_datetime = new moment(event_date_raw + ' ' + event_time_raw, 'MMMM D YYYY h:mm a').toDate();
+            events.push({title, venue, url, date_and_time: event_datetime, price: event_price_raw});
+        });
+        //console.log(events);
+        return events;
+    })
+    .catch(console.error);
+}
+
+module.exports.scrapeFillmore = scrapeFillmore;
+
+/*
+scrapeFillmore().then(data => {
+    console.log(data);
+});
+*/
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let scraper = require('./concert-scraper.js');
 var http = require('http');
 const { pool } = require('./config');
 
@@ -12,9 +13,28 @@ function getAllConcerts() {
     });
 };
 
+function getConcertDiff() {
+    scraper.scrapeFillmore().then(data => {
+        pool.query('SELECT * from concerts WHERE venue = ($1) ORDER BY title, venue, date_and_time', ["Fillmore"], (err, res) => {
+            if (err) {
+                console.log(err.stack);
+            } else {
+                let newEvents = [];
+                let existingEvents = new Set(res.rows);
+                data.forEach(event => {
+                    if (!existingEvents.has(event)) {
+                        newEvents.push(event);
+                    }
+                });
+                console.log(newEvents);
+            }
+        });
+    });
+}
+
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    getAllConcerts();
+    getConcertDiff();
     res.end();
 }).listen(process.env.PORT || 8080);
 
