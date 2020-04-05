eec5c76e7e0d93d5006ba610a8d0af26322f56c3
Bill Kwai <billkwai.edu@gmail.com>
2020-04-05 15:19:13 -0700

    makes cron job http request synchronous

--- bin/worker.js
+++ bin/worker.js
@@ -2,7 +2,7 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const Http = new XMLHttpRequest();
 const url='https://concert-scraper.herokuapp.com/concerts/update';
-Http.open("GET", url);
+Http.open("GET", url, false);
 Http.send();
 
 Http.onreadystatechange = (e) => {
